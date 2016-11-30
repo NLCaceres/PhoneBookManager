475a73ba646d373934e60c006b3f04489717dd89
NLCaceres <nlcaceres21@gmail.com>
2016-11-30 14:50:52 -0800

    Initial Commit

---
+++ PhoneBookManagerTests/PhoneBookManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhoneBookManagerTests.swift
+//  PhoneBookManagerTests
+//
+//  Created by Nicholas L Caceres on 11/30/16.
+//  Copyright © 2016 Nicholas L Caceres. All rights reserved.
+//
+
+import XCTest
+@testable import PhoneBookManager
+
+class PhoneBookManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
